59c0ae826d636aefb5e78e1318c82446646c423e
choizlor <choizlor@gmail.com>
2023-01-27 01:11:43 +0900

    FEAT: 상단/하단바 jsx 작성

--- frontend/src/components/Nav/Topnav.jsx
+++ frontend/src/components/Nav/Topnav.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./TopNav.module.css";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BellIcon } from "@heroicons/react/24/solid";
 
 export default function TopNav() {
   return (
-    <nav>
-      zum:go |
-      <Link to="/search">search</Link>
+    <nav className={styles.body}>
+      <Link to="/">
+        <p className={styles.logo}>zum:go</p>
+      </Link>
+      <Link to="/search">
+        <MagnifyingGlassIcon className={styles.icon} />
+      </Link>
+      <BellIcon className={styles.icon}/>
     </nav>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./styles/Login.module.css";
-import kakaoBtn from '../assets/images/kakao_login_medium_wide.png';
+import kakaoBtn from "../assets/images/kakao_login_medium_wide.png";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 export default function Login() {
-
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  console.log(REST_API_KEY)
+  console.log(REST_API_KEY);
   const REDIRECT_URI = "http://localhost:3000/oauth";
 
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -13,31 +13,13 @@ export default function Login() {
   return (
     <div className={styles.body}>
       <div className={styles.nav}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="white"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-
+        <ChevronLeftIcon className="w-6 h-6 text-gray-100" />
         <div className={styles.title}>로그인 하기</div>
       </div>
       <div className={styles.logo}>zum:go</div>
       <a href={KAKAO_AUTH_URI}>
-        <img 
-          className={styles.kakaoBtn} 
-          src={kakaoBtn} 
-        />
+        <img className={styles.kakaoBtn} src={kakaoBtn} />
       </a>
-        
     </div>
   );
 }
