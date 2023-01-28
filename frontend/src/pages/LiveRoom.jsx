79d3e23dee89d33c928095de41b89f90bc0eb480
yuna872 <yunae426@gmail.com>
2023-01-28 16:30:20 +0900

    FEAT: 라이브방 채팅 입력폼 생성

--- frontend/src/pages/LiveRoom.jsx
+++ frontend/src/pages/LiveRoom.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './styles/LiveRoom.module.css';
+import { ArrowUpCircleIcon } from '@heroicons/react/24/solid'
 
 export default function LiveRoom() {
+    
+    const [inputData, setInputData] = useState("");
+    
+
+    const handleChange = e => {
+        console.log(e.target.value)
+        setInputData(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('입력한 내용 전송:', inputData)
+        setInputData('');
+        // 채팅 내용 전송하기
+    }
     return (
-        <div>
-            라이브 방
+        <div className={styles.body}>
+            <div className={styles.top}>
+
+            </div>
+            <div className={styles.bottom}>
+                <div className={styles.chats}>
+
+                </div>
+                <div className={styles.bottombar}>
+                    <form className={styles.chatform} onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={inputData}
+                            className={styles.input}
+                            placeholder="댓글 달기"
+                            onChange={handleChange}
+                        />
+                        <button type="submit">
+                            <ArrowUpCircleIcon/>
+                        </button>
+                    </form>
+                    <button className={styles.gobtn}></button>
+                </div>
+            </div>
         </div>
-    ); 
+    );
 }
 
