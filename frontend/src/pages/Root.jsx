c81319d2531493f61a3b52766112c8e6a0ed8cd8
yuna872 <yunae426@gmail.com>
2023-01-29 19:57:47 +0900

    BUG: 카카오 로그인, stirct mode 해제

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Root() {
+
+    const token = window.localStorage.getItem('token');
+
+    try {
+        const res = axios.post('api/me', data, {
+            headers: {
+                Authorization: token,
+            },
+        });
+    } catch (err) {
+        console.log(err)
+    }
+
     return (
         <div>
             <Outlet />
