8004d4de4af257b51f87a17c2f2f5e542e0e3dda
yuna kim <yunae426@gmail.com>
2023-01-29 20:56:50 +0900

    STYLE: liveroom css

--- frontend/src/pages/LiveRoom.jsx
+++ frontend/src/pages/LiveRoom.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./styles/LiveRoom.module.css";
 import { ArrowUpCircleIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import testImg from "../assets/images/kim.png";
 
 export default function LiveRoom() {
   const [inputData, setInputData] = useState("");
@@ -19,41 +20,47 @@ export default function LiveRoom() {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        {/* 상단 컨테이너 */}
-        <div className={styles.top}>
-          <div className={styles.topbar}>
-            {/* 상단 왼쪽 판매자 정보 */}
-            <div className={styles.topleft}>
-              <div className={styles.sellerimg}></div>
-              <div className={styles.sellername}>냠냠이 님</div>
-            </div>
-            {/* 상단 오른쪽 */}
-            <div className={styles.topright}>
-              <div className={styles.live}>LIVE</div>
-              <XMarkIcon className={styles.exiticon} />
+        <div className={styles.toplinear}>
+          {/* 상단 컨테이너 */}
+          <div className={styles.top}>
+            <div className={styles.topbar}>
+              {/* 상단 왼쪽 판매자 정보 */}
+              <div className={styles.topleft}>
+                <div className={styles.sellerimg}>
+                  <img src={testImg} alt="" />
+                </div>
+                <div className={styles.sellername}>냠냠이 님</div>
+              </div>
+              {/* 상단 오른쪽 */}
+              <div className={styles.topright}>
+                <div className={styles.live}>LIVE</div>
+                <XMarkIcon className={styles.exiticon} />
+              </div>
             </div>
           </div>
         </div>
         {/* 하단 컨테이너 */}
-        <div className={styles.bottom}>
-          <div className={styles.chats}>채팅목록</div>
-          <div className={styles.bottombar}>
-            <form className={styles.chatform} onSubmit={handleSubmit}>
-              <div>
-                <input
-                  type="text"
-                  value={inputData}
-                  className={styles.input}
-                  placeholder="댓글 달기"
-                  onChange={handleChange}
+        <div className={styles.bottomlinear}>
+          <div className={styles.bottom}>
+            <div className={styles.chats}>채팅목록</div>
+            <div className={styles.bottombar}>
+              <form className={styles.chatform} onSubmit={handleSubmit}>
+                <div>
+                  <input
+                    type="text"
+                    value={inputData}
+                    className={styles.input}
+                    placeholder="댓글 달기"
+                    onChange={handleChange}
+                  />
+                </div>
+                <ArrowUpCircleIcon
+                  className={styles.submitbtn}
+                  onClick={handleSubmit}
                 />
-              </div>
-              <ArrowUpCircleIcon
-                className={styles.submitbtn}
-                onClick={handleSubmit}
-              />
-            </form>
-            <button className={styles.gobtn}>GO?</button>
+              </form>
+              <button className={styles.gobtn}>GO?</button>
+            </div>
           </div>
         </div>
       </div>
