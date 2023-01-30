774fe34076bf10f6d0db237814a3ec7a62cf5be1
psihyeong <qkrtlgud3286@naver.com>
2023-01-30 09:44:00 +0900

    FEAT: JPA entity 구현

---
+++ backend/src/main/java/com/isf6/backend/db/entity/LiveStatus.java
@@ -0,0 +1,5 @@
+package com.isf6.backend.db.entity;
+
+public enum LiveStatus {
+    ONAIR, WAIT, CLOSED
+}
---
+++ backend/src/main/java/com/isf6/backend/db/entity/User.java
@@ -0,0 +1,37 @@
+package com.isf6.backend.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    private String token;
+
+    private String photo;
+
+    private int point;
+
+    @OneToMany(mappedBy = "user")
+    private List<Product> products = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<UserLive> userLives = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<LiveRequest> liveRequests = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Wish> wishes = new ArrayList<>();
+}
