0987319bc45433087f9b3e940f43755e361f03c0
나혜승 <nahs00122@gmail.com>
2023-01-30 10:01:10 +0900

    REMOVE: store import 삭제

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 
 // store import
-import store from './store/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
         <App />
-    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
