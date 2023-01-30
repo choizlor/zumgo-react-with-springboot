76d203e20246853c3c018562d3c07f9ca7eb2073
jeonghyo <xozo5939@gmail.com>
2023-01-30 15:20:47 +0900

    FIX: chat open source test 버그 수정

--- backend/src/main/java/com/isf6/backend/config/WebSocketConfiguration.java
+++ backend/src/main/java/com/isf6/backend/config/WebSocketConfiguration.java
@@ -1,8 +1,10 @@
 package com.isf6.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+@Component
 public class WebSocketConfiguration {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
