22c37b45ac74a77ca34fc462d962f6dde424570c
yuna <yunae426@gmail.com>
2023-01-31 23:00:32 +0900

    FEAT: product read, update axios 요청 완

--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -1,37 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/Detail.module.css";
 import LiveBtn from "../components/Detail/LiveBtn";
-import z from '../assets/images/z.png';
+import z from "../assets/images/z.png";
 import DetailModal from "../components/Detail/DetailModal";
+import { useNavigate, useParams } from "react-router";
 
 // heroicons
-import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid'
-import { HeartIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import {
+  HeartIcon,
+  ChevronLeftIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/outline";
 // swiper - 이미지 슬라이더
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 import { Navigation, Pagination } from "swiper";
+import axios from "axios";
 
 export default function Detail() {
-
+  const params = useParams();
+  const productId = params.productId;
+  const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
 
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/product/${productId}`)
+    .then((res) => { 
+      // 상세 정보 불러오기
+      setProduct(res.data)
+    })
+    .catch((err) => { console.log(err)});
+  })
 
   return (
     <div className={styles.body}>
       {/* 상품 이미지 배너 */}
       <div className={styles.swiperbox}>
         {/* <ChevronLeftIcon className="w-6 h-6 text-gray-100" /> */}
-
-        <Swiper 
-          className={styles.swiper} 
-          navigation={true} 
+        <Swiper
+          className={styles.swiper}
+          navigation={true}
           pagination={true}
-          loop={true} 
-          modules={[Navigation, Pagination]}>
+          loop={true}
+          modules={[Navigation, Pagination]}
+        >
           <SwiperSlide>
             <img
               src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshopping.phinf.naver.net%2Fmain_3218672%2F32186720809.20220505182637.jpg&type=a340"
@@ -63,7 +81,6 @@ export default function Detail() {
           <span>1/24 16시</span>
           <span>LIVE 예정</span>
         </div>
-
       </div>
       {/* 상품 정보 container */}
       <div className={styles.container}>
@@ -80,25 +97,34 @@ export default function Detail() {
         <select
           className={styles.dropdown}
           onChange={(e) => {
-            if (e.target.value === '거래완료') {
-              setModalOpen(true)
+            if (e.target.value === "거래완료") {
+              setModalOpen(true);
             } else {
-              setModalOpen(false)
+              setModalOpen(false);
             }
           }}
-          name="" id=""
+          name=""
+          id=""
         >
           <option value="판매 중">판매 중</option>
           <option value="예약 중">예약 중</option>
           <option value="거래완료">거래완료</option>
         </select>
-        <div className={styles.title}>나이키 레거시 모자 판매합니다</div>
-        <div className={styles.price}>10,000원</div>
-        <div className={styles.desc}>
-          제 머리가 큰 탓에 거의 착용도 못해 본,,, 그냥 새제품입니다.. 근데 이제
-          포장지는 없는,, 라이브 요청 하시면 라이브도 가능합니다.. 저는
-          취준생이걸랑요,,
-        </div>
+        {/*  판매자에게만 수정하기 버튼이 보임*/}
+        {true ? (
+          <div className={styles.canedit}>
+            <div className={styles.title}>{product.title}</div>
+            <PencilSquareIcon className={styles.editbtn} onClick={() => {navigate(`/update/${productId}`, {
+              state: product
+            })}}/>
+          </div>
+        ): (
+          <div className={styles.title}>{product.title}</div>
+
+        )}
+
+        <div className={styles.price}>{product.price}원</div>
+        <div className={styles.desc}>{product.description}</div>
         <div className={styles.icons}>
           <div className={styles.zbox}>
             <div className={styles.z}>
@@ -118,9 +144,7 @@ export default function Detail() {
         <div className={styles.timeBox}>
           <span className={styles.timeTitle}>Live 가능 시간대 :</span>
           <div className={styles.timeContent}>
-            <span>
-              매일 오전 8시 이후
-            </span>
+            <span>{product.reservation}</span>
           </div>
         </div>
         <LiveBtn />
@@ -129,4 +153,3 @@ export default function Detail() {
     </div>
   );
 }
-
---
+++ frontend/src/pages/UpdateProduct.jsx
@@ -0,0 +1,104 @@
+import { useLocation, useNavigate } from "react-router";
+import React, { useState } from "react";
+import styles from "./styles/AddProduct.module.css";
+import { ChevronLeftIcon, CameraIcon } from "@heroicons/react/24/solid";
+import axios from "axios";
+import testImg from "../assets/images/kim.png";
+// import { useSelector } from "react-redux";
+
+export default function UpdateProduct() {
+  // redux 사용하기
+  // const user = useSelector((state) => { return state.user});
+  const location = useLocation();
+  const navigate = useNavigate();
+  const product = location.state;
+
+  const [title, setTitle] = useState(product.title);
+  const [price, setPrice] = useState(product.price);
+  const [description, setDescription] = useState(product.description);
+  const [reservation, setReservation] = useState(product.reservation);
+  const [photo, setPhoto] = useState([]);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handlePriceChange = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const handleReservationChange = (e) => {
+    setReservation(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  // 수정하기 api 요청
+  const handleUpdate = () => {
+    axios.put(`http://localhost:8080/product/${product.id}`,{
+        title,
+        price,
+        description,
+        reservation : '2010-10-14',
+        photo : '아직이용',
+    })
+    .then((res) => { navigate(`/detail/${product.id}`)})
+    .catch((err) => { console.log(err)});
+  };
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.nav}>
+        <ChevronLeftIcon className="w-6 h-6 text-black-100" />
+        <div className={styles.title}>상품 정보 수정하기</div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.button}>
+          <CameraIcon className={styles.camera} />
+          <div className={styles.num}>0/5</div>
+        </div>
+        <input
+          className={styles.file}
+          type="file"
+          accept="image/*"
+          capture="camera"
+          multiple
+        />
+        {/* <div className={styles.addbtn}> */}
+        <input
+          className={`${styles.input} ${styles.titleinput}`}
+          onChange={handleTitleChange}
+          type="text"
+          placeholder="제목"
+          defaultValue={product.title}
+        />
+        <input
+          className={styles.input}
+          onChange={handlePriceChange}
+          type="number"
+          placeholder="$ 가격 (0원 가능)"
+          defaultValue={product.price}
+        />
+        <textarea
+          className={styles.textarea}
+          onChange={handleReservationChange}
+          placeholder="라이브 가능 시간 &#13;(ex - 10:00~12:00, 18:00~19:00)"
+          value={reservation}
+        >
+        </textarea>
+        <textarea
+          className={`${styles.textarea} ${styles.descinput}`}
+          onChange={handleDescriptionChange}
+          placeholder="상품 설명(300자 이내)"
+          value={description}
+        >
+        </textarea>
+        <div className={styles.addbtn} onClick={handleUpdate}>
+          <span>수정하기</span>
+        </div>
+      </div>
+    </div>
+  );
+}
