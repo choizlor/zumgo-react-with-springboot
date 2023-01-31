884874dfd00b8dccae7574ba348ff32d15a64d75
choizlor <choizlor@gmail.com>
2023-01-31 17:58:17 +0900

    STYLE: react datepicker 추가

--- frontend/src/components/Live/ReservationModal.jsx
+++ frontend/src/components/Live/ReservationModal.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./ReservationModal.module.css";
+import DatePicker from "react-datepicker";
+import { ko } from "date-fns/esm/locale";
+import "react-datepicker/dist/react-datepicker.css";
+
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import styles from './ReservationModal.module.css'
 
 export default function ReservationModal({ setModalOpen }) {
+  const [selectDate, setSelectDate] = useState(new Date());
+
   // 모달 끄기
   const closeModal = () => {
     setModalOpen(false);
   };
 
+  const handleSubmit = () => {
+    setModalOpen(false);
+  };
   return (
     <div className={styles.container}>
+      <button onClick={handleSubmit}>예약하기</button>
       <XMarkIcon onClick={closeModal} className={styles.close} />
-      <p>날짜, 시간 선택</p>
+      <div className={styles.date}>
+        <DatePicker
+          locale={ko}
+          selected={selectDate}
+          onChange={(date) => setSelectDate(date)}
+          showTimeInput
+          dateFormat="Pp"
+          minDate={new Date()}
+          popperModifiers={{
+            // 모바일 web 환경에서 화면을 벗어나지 않도록 하는 설정
+            preventOverflow: {
+              enabled: true,
+            },
+          }}
+          
+        />
+      </div>
     </div>
   );
 }
