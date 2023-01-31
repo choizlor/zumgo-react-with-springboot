e0852056a552a446728d744363e4cfc27048fd4d
나혜승 <nahs00122@gmail.com>
2023-01-31 23:02:29 +0900

    FEAT:상품 리스트 필터 컴포넌트 나누기

---
+++ frontend/src/components/SellList/Status.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Status ({filters,filter,onFilterChange}) {
+    return <Status>
+        <ul>
+            {filters.map()}
+
+        </ul>
+
+    </Status>
+
+
+};
--- frontend/src/pages/SellList.jsx
+++ frontend/src/pages/SellList.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./styles/SellList.module.css";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import SellListItems from "../components/SellList/SellListItems";
+import Status from "../components/SellList/Status";
 
 export default function SellList() {
-    return (
-        <div>
-            판매 목록
-        </div>
-    );
-}
+  const filters = ['onsale','inprogess','soldout']
+  const [filter,setFilter] = useState(filters[0]);
 
+  return (
+    //전체 컨테이너
+    <div className={styles.body}>
+      {/**nav부분  */}
+      <div className={styles.nav}>
+        <ChevronLeftIcon className="w-6 h-6 text-black-100" />
+        <div className={styles.title}>판매 목록</div>
+      </div>
+      {/*거래 상태 표시 */}
+      <div>
+        <Status filters={filters} filter={filter} onFilterChange={setFilter}/>
+        <SellListItems filter={filter} />
+      </div>
+    </div>
+  );
+}
