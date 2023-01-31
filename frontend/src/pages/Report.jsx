04238766b7e76d0ac0431233832de86764c27c1b
나혜승 <nahs00122@gmail.com>
2023-01-31 21:04:21 +0900

    FEAT: 신고하기 페이지 css완성

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./styles/Report.module.css";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 export default function Report() {
-    return (
-        <div>
-            신고하기
+  const [userId, setuserID] = useState('')
+  const [comment,setComment] = useState('')
+  const report = () => {
+    const context = {
+      userId: "userId",
+      comment: "comment"
+    }
+    axios
+    .post('http://localhost:8080/report/', context)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+  return (
+    <div className={styles.body}>
+      <div className={styles.nav}>
+        <ChevronLeftIcon className="w-6 h-6 text-black-100" />
+        <div className={styles.title}>신고하기</div>
+      </div>
+      <div className={styles.reportform}>
+        <div className={styles.title}>
+          뇸뇸이 님을 신고하는 이유를 작성해주세요.
         </div>
-    );
+        <textarea
+          className={styles.contents}
+          cols="30"
+          rows="10"
+          placeholder="신고 하는 이유를 상세히 기재해 주세요."
+        ></textarea>
+      </div>
+      <div className={styles.button} onClick={report}>작성완료</div>
+    </div>
+  );
 }
-
