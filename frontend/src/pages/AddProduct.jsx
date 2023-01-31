a0165771f8e29fcc645813505dcbad923461a84f
나혜승 <nahs00122@gmail.com>
2023-01-31 15:14:36 +0900

    FEAT:파일 업로드 버튼 생성

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styles from "./styles/AddProduct.module.css"
+import { ChevronLeftIcon, CameraIcon } from "@heroicons/react/24/solid";
+
 
 export default function AddProduct() {
     return (
-        <div>
-            상품 등록 페이지
-        </div>
+        <div className={styles.body}>
+          <div className={styles.nav}>
+            <ChevronLeftIcon className="w-6 h-6 text-black-100" />
+            <div className={styles.title}>상품 등록하기</div>
+          </div>
+              <div className={styles.button}>
+                <CameraIcon className={styles.camera}/>
+                <div className={styles.num}>0/5</div>
+              </div>
+              <input
+              className={styles.file}
+                type="file"
+                accept="image/*"
+                capture="camera"
+                multiple
+              />
+                
+          </div>
     );
 }
 
