ef91db836070b8b0b63490235d8cd8897d9a7cf0
jeonghyo <xozo5939@gmail.com>
2023-02-01 21:20:59 +0900

    CHORE: aws에 db연결하는 설정 파일 변경

--- backend/src/main/java/com/isf6/backend/config/handler/StompHandler.java
+++ backend/src/main/java/com/isf6/backend/config/handler/StompHandler.java
@@ -28,12 +28,12 @@ public class StompHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        if (StompCommand.CONNECT == accessor.getCommand()) { // websocket 연결요청
+        if (StompCommand.CONNECT == accessor.getCommand()) { // websocket 연결요청, 로그인 여부 확인
             String jwtToken = accessor.getFirstNativeHeader("AUTH");
             log.info("CONNECT {}", jwtToken);
             // Header의 jwt token 검증
 //            jwtTokenProvider.validateToken(jwtToken);
-        } else if (StompCommand.SUBSCRIBE == accessor.getCommand()) { // 채팅룸 구독요청
+        } else if (StompCommand.SUBSCRIBE == accessor.getCommand()) { // 채팅룸 구독요청, user가 일치하는지 확인하고 일치한다면 채팅방 목록에 추가함.
             // header정보에서 구독 destination정보를 얻고, roomId를 추출한다.
             String roomId = chatService.getRoomId(Optional.ofNullable((String) message.getHeaders().get("simpDestination")).orElse("InvalidRoomId"));
             // 채팅방에 들어온 클라이언트 sessionId를 roomId와 맵핑해 놓는다.(나중에 특정 세션이 어떤 채팅방에 들어가 있는지 알기 위함)
