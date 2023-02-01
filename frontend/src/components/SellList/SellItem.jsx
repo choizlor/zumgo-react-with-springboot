80fa0e504fde501e77a3d639bd8a40fafb6d8d5f
나혜승 <nahs00122@gmail.com>
2023-02-01 17:41:13 +0900

    FEAT:JSON 따옴표 제거

--- frontend/src/components/SellList/SellItem.jsx
+++ frontend/src/components/SellList/SellItem.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import styles from "./SellItem.module.css";
 import { Link } from "react-router-dom";
 export default function SellItem({ product }) {
+  const image = JSON.stringify(product.image).replace (/"/g,'');
+  const name =  JSON.stringify(product.name).replace (/"/g,'');
+  const price = JSON.stringify(product.price).replace (/"/g,'');
+  const status = JSON.stringify(product.status).replace (/"/g,'');
   return (
     <div className={styles.body}>
       <Link to="/detail/:productId"></Link>
       <div className={styles.image}>
-      {JSON.stringify(product.image)}
+      {image}
       </div>
       <div className={styles.name}>
-      {JSON.stringify(product.name)}
+      {name}
       </div>
       <div className={styles.price}>
-      {JSON.stringify(product.price)}
+      {price}
       </div>
       <div className={styles.status}>
-      {JSON.stringify(product.status)}
+      {status}
       </div>
     </div>
   );
--- frontend/src/components/SellList/SellItems.jsx
+++ frontend/src/components/SellList/SellItems.jsx
@@ -6,8 +6,8 @@ import SellItem from './SellItem';
 export default function SellItems({filter}) {
   //총 상품 목록들 
   const [products, setProducts] = useState([
-    {id:'123',name:'가방',price:'8000',status:'onsale',image:'이미지요~'},
-    {id:'124',name:'옷',price:'7000',status:'inprogress',image:'이미지입니다~'}
+    {id:'123',name:'버즈 프로 2 미개봉 새상품',price:'8000원',status:'onsale',image:'이미지요~'},
+    {id:'124',name:'아이패드 팔아요',price:'500,000원',status:'inprogress',image:'이미지입니다~'}
   ]);
   const filtered = getFilteredItems(products,filter)
         return (
