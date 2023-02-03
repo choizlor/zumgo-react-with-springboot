98db7fdce67f5f91d5f6d35d179a6d34ade21553
yuna <yunae426@gmail.com>
2023-02-04 00:51:17 +0900

    STYLE: 홈 화면 css 수정

--- frontend/src/components/Home/HomeBanner.jsx
+++ frontend/src/components/Home/HomeBanner.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './HomeBanner.module.css'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./HomeBanner.module.css";
 
 export default function HomeBanner() {
-    return (
-        <div className={styles.box}>
-            <p className={styles.font}>중고 거래,</p>
-            <p className={styles.font}>줌고로 생생하게!</p>
-            <button className={styles.loginbtn}><Link to="/login">로그인하기</Link></button>
-        </div>
-    );
+  const navigate = useNavigate();
+  return (
+    <div className={styles.body}>
+      <div className={styles.leftbox}>
+        <div className={styles.font}>중고 거래,</div>
+        <div className={styles.font}>줌고로 생생하게!</div>
+        <button
+          className={styles.loginbtn}
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          로그인하기
+        </button>
+      </div>
+      <div className={styles.rightbox}></div>
+    </div>
+  );
 }
