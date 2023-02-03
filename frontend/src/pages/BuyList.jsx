001479b05ab7007c208403434e48de73f652a5a2
choizlor <choizlor@gmail.com>
2023-02-04 02:19:29 +0900

    FEAT: user 판매목록 조회

--- frontend/src/pages/BuyList.jsx
+++ frontend/src/pages/BuyList.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import styles from "./styles/BuyList.module.css";
+import { useNavigate, useParams } from "react-router";
 
 export default function BuyList() {
-    return (
-        <div>
-            구매 목록
-        </div>
-    );
-}
+  const navigate = useNavigate();
+  const userId = useParams().userId;
+  const [products, setProducts] = useState();
 
+  // useEffect(() => {
+  //   axios
+  //     .get("")
+  //     .then((res) => {
+  //       setProducts(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.nav}>
+        <ChevronLeftIcon
+          className="w-6 h-6 text-black-100"
+          onClick={() => {
+            navigate(`/userinfo/${userId}`);
+          }}
+        />
+        <div className={styles.title}>구매 목록</div>
+      </div>
+        {/* <div className={styles.scrollarea}>
+          {products?.map((product) => {
+            return (
+              <ProductItem
+                key={product.productId}
+                product={product}
+                clickProduct={clickProduct}
+              />
+            );
+          })}
+        </div> */}
+    </div>
+  );
+}
