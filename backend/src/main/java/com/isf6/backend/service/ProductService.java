120ccacea0660530d85dc08ab0ac58bbac49fb71
psihyeong <qkrtlgud3286@naver.com>
2023-02-06 17:22:14 +0900

    FIX: response파일 수정

--- backend/src/main/java/com/isf6/backend/service/ProductService.java
+++ backend/src/main/java/com/isf6/backend/service/ProductService.java
@@ -124,7 +124,6 @@ public class ProductService {
         return true;
     }
 
-<<<<<<< HEAD
 //    public Page<IndexProductsResDto> getMainProducts(String sort, String category, int page, int size) {
 //        Pageable pageable = PageRequest.of(page, size);
 //        return productRepositoryCustomImpl.findAllByCategoryOrderBySort(sort, category, pageable);
@@ -141,11 +140,9 @@ public class ProductService {
     }
 
 
-=======
     // 문자열 포함한 상품 목록 검색
     public List<Product> findProducts(String productSearch) {
         return productSearchRepository.findBySearch(productSearch);
     }
 
->>>>>>> eb419e5b5dd3a1b1539f370ee07ba96d365bcde1
 }
