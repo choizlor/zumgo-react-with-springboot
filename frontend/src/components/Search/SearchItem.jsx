457ff181e36b075f8f7569e772faaf10826c53b9
나혜승 <nahs00122@gmail.com>
2023-02-06 11:24:04 +0900

    STYLE:최근 검색어 이단 박스 만들기

--- frontend/src/components/Search/SearchItem.jsx
+++ frontend/src/components/Search/SearchItem.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import styles from "./SearchItem.module.css";
 
-export default function SearchItem() {
-    return (
-        <div>
+export default function SearchItem({ word }) {
+  const { content } = word;
+  return (
+      <div className={styles.word}>
+        <span>{content}</span>
+        <div className={styles.icon}>
+          <XMarkIcon />
         </div>
-    );
+      </div>
+  );
 }
-
--- frontend/src/components/Search/SearchItems.jsx
+++ frontend/src/components/Search/SearchItems.jsx
@@ -3,17 +3,20 @@ import { useState } from 'react';
 import SearchItem from './SearchItem';
 import styles from './SearchItems.module.css'
 
+
 export default function SearchItems() {
     const [words, setwords] = useState([ 
     { id:1, content:'바지' },
-    { id:2, content:'모자' }]);
+    { id:2, content:'모자' },
+    { id:3, content:'의자' },
+    { id:4, content:'컴퓨터' },]);
     return (
         <div className={styles.body}>
             <span className={styles.text}>최근 검색어</span>
-            <ul>
+            <ul className={styles.wordcontainer}>
                 {words.map((word) => (
                     <SearchItem
-                    key={word.wordId}
+                    key={word.id}
                     word={word}/>
                 ))}
             </ul>
