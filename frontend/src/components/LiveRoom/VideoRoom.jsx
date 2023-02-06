533be94df6918b0c7b5bfe488016aa02ca7d76aa
yuna <yunae426@gmail.com>
2023-02-07 01:34:28 +0900

    STYLE : videoroom css

--- frontend/src/components/LiveRoom/VideoRoom.jsx
+++ frontend/src/components/LiveRoom/VideoRoom.jsx
@@ -393,6 +393,15 @@ const VideoRoomTest = () => {
               {/* {!isHost && <UserVideoComponent streamManager={subscribers} />} */}
             </div>
           ) : null}
+          
+
+          {/* 배경 그라데이션 */}
+          <div className={styles.background}>
+            <div className={styles.bgtop}></div>
+            <div className={styles.bgbottom}></div>
+          </div>
+
+          {/* 라이브 화면 */}
           <div className={styles.top}>
             <div className={styles.toptop}>
               <div className={styles.topleft}>
@@ -475,14 +484,14 @@ const VideoRoomTest = () => {
             ) : null}
           </div>
           <div>
-            {true ? (
+            {celebrity ? (
               <div className={styles.modal}>
-                <div>
+                <div className={styles.modaltitle}>
 
                 축하합니다! 
                 </div>
                 <div className={styles.modalimg}>
-                  <img src="" alt="" />
+                  <img src={userImg} alt="" />
                 </div>
                 <div className={styles.modalbiddername}>딸기우유 서녕 님이,</div>
                 <div className={styles.modalbidprice}>50300원에 낙찰!</div>
