2a1cc2a3fa9901a2513542c31776bfba6689ee47
choizlor <choizlor@gmail.com>
2023-02-09 20:44:28 +0900

    FIX: 라이브 생성 axios

--- frontend/src/components/Live/ReservationModal.jsx
+++ frontend/src/components/Live/ReservationModal.jsx
@@ -21,6 +21,8 @@ export default function ReservationModal({ setModalOpen, productId }) {
     setModalOpen(false);
   };
 
+  console.log(typeof(productId), '🥱product type')
+
   // 상품정보 불러오기
   useEffect(() => {
     axios
@@ -50,17 +52,22 @@ export default function ReservationModal({ setModalOpen, productId }) {
       .catch((err) => console.log(err));
 
     axios
-      .post(`https://i8c110.p.ssafy.io/api/v1/live/room`, {
-        headers: {
-          Authorization: token,
-          "Content-Type": "application/json",
+      .post(
+        `https://i8c110.p.ssafy.io/api/v1/live/room`,
+        {
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
         },
-        body: {
-          productId: productId,
-          liveStartTime: reserve,
-          liveStatus: "WAIT",
-        },
-      })
+        {
+          body: {
+            productId: productId,
+            liveStartTime: reserve,
+            liveStatus: "WAIT",
+          },
+        }
+      )
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
