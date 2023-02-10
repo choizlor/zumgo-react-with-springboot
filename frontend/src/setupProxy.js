45344c4ff4bc4e1917e47fcc455611d63130478f
yuna <yunae426@gmail.com>
2023-02-10 12:02:36 +0900

    FIX: 나에게 달린 리뷰 axios 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    "wss",
+    "ws",
     createProxyMiddleware({
       target: "https://i8c110.p.ssafy.io",
       ws: true,  // 웹소켓을 사용하겠다!
