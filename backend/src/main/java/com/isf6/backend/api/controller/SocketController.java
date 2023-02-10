c865fa6bfc79a241facfc981434b893f505b0f2c
psihyeong <qkrtlgud3286@naver.com>
2023-02-11 00:00:32 +0900

    FIX: 채팅방조회API 프로필사진 조회 추가

--- backend/src/main/java/com/isf6/backend/api/controller/SocketController.java
+++ backend/src/main/java/com/isf6/backend/api/controller/SocketController.java
@@ -7,8 +7,10 @@ import com.isf6.backend.api.Response.ChatInfoResDto;
 import com.isf6.backend.api.Response.ChatRoomInfoResDto;
 import com.isf6.backend.domain.entity.Chat;
 import com.isf6.backend.domain.entity.ChatRoom;
+import com.isf6.backend.domain.entity.User;
 import com.isf6.backend.domain.repository.ChatRepository;
 import com.isf6.backend.domain.repository.ChatRoomRepository;
+import com.isf6.backend.domain.repository.UserRepository;
 import com.isf6.backend.service.SocketService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
@@ -101,6 +103,10 @@ public class SocketController {
             chatInfo.setChatRoomId(chatRoomId);
         }
 
+        // 프로필 이미지 추가
+        chatInfo.setSellerImg(chatRoomInfo.getSeller().getKakaoProfileImg());
+        chatInfo.setBuyerImg(chatRoomInfo.getBuyer().getKakaoProfileImg());
+        
         return chatInfo;
     }
 
