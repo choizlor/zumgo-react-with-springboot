fa2badecc83984887441b7a48e1422249ff9414f
choizlor <choizlor@gmail.com>
2023-02-11 19:48:27 +0900

    FIX: axios 변경

--- frontend/src/components/Live/LiveCard.jsx
+++ frontend/src/components/Live/LiveCard.jsx
@@ -15,7 +15,7 @@ export default function LiveCard({ product, clickLive }) {
         <button
           className={styles.btn}
           onClick={() => {
-            clickLive(product.id);
+            clickLive(product.productId);
           }}
         >
           라이브 입장하기
