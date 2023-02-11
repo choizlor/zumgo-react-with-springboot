56a579f247aeb8f4e71d905f289113bac300f6ce
choizlor <choizlor@gmail.com>
2023-02-11 13:33:36 +0900

    STYLE: 방송중 axios

--- frontend/src/components/Live/BuyLive.jsx
+++ frontend/src/components/Live/BuyLive.jsx
@@ -4,7 +4,7 @@ import styles from "./BuyLive.module.css";
 import LiveCard from "./LiveCard";
 import RequestLive from "./RequestLive";
 
-export default function BuyLive({ myLiveRequestList }) {
+export default function BuyLive({ onairList, myLiveRequestList }) {
   const requestList = myLiveRequestList?.filter((product) => {
     return product.reserve !== null;
   });
@@ -13,9 +13,9 @@ export default function BuyLive({ myLiveRequestList }) {
     <div>
       <div className={styles.text}>방송 중</div>
       <div>
-        {/* {onairList?.map((product) => {
+        {onairList?.map((product) => {
           <LiveCard key={product.id} product={product} />;
-        })} */}
+        })}
       </div>
       <div className={styles.text}>라이브 예정</div>
       <div>
