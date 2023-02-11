3c211e1fd5afaad1ca9ba3426ac6a302d920db99
choizlor <choizlor@gmail.com>
2023-02-11 13:40:32 +0900

    STYLE: 방송중 탭 정보

--- frontend/src/components/Live/BuyLive.jsx
+++ frontend/src/components/Live/BuyLive.jsx
@@ -1,10 +1,17 @@
 import axios from "axios";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./BuyLive.module.css";
 import LiveCard from "./LiveCard";
 import RequestLive from "./RequestLive";
 
 export default function BuyLive({ onairList, myLiveRequestList }) {
+  const navigate = useNavigate();
+
+  const clickLive = (productId) => {
+    navigate(`live/${productId}`);
+  };
+
   const requestList = myLiveRequestList?.filter((product) => {
     return product.reserve !== null;
   });
@@ -14,7 +21,7 @@ export default function BuyLive({ onairList, myLiveRequestList }) {
       <div className={styles.text}>방송 중</div>
       <div>
         {onairList?.map((product) => {
-          <LiveCard key={product.id} product={product} />;
+          <LiveCard key={product.id} product={product} clickLive={clickLive} />;
         })}
       </div>
       <div className={styles.text}>라이브 예정</div>
