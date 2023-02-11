a949e395c62a27fee0fe90549cd198b8d08df3bc
나혜승 <nahs00122@gmail.com>
2023-02-11 19:49:45 +0900

    FEAT: 리뷰 CSS 중간 수정

--- frontend/src/pages/UserInfo/ReviewItem.jsx
+++ frontend/src/pages/UserInfo/ReviewItem.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./ReviewItem.module.css";
 
-export default function ReviewItem({review}) {
-  console.log(review)
+export default function ReviewItem({ review }) {
+  console.log(review);
   return (
     <div className={styles.body}>
+      <div className={styles.review}>
+        <div className={styles.userimg}>
+          <img src={review.buyer.kakaoProfileImg} />
+        </div>
+        <div className={styles.userinfo}>
+          <div className={styles.writer}>{review.buyer.kakaoNickname}</div>
+          <div className={styles.comment}>{review.review}</div>
+        </div>
+      </div>
       <div className={styles.writer}>{review.buyer.kakaoNickname}</div>
       <div className={styles.comment}>{review.review}</div>
     </div>
