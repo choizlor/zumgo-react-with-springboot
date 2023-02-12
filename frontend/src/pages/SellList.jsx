e1d0eb8b41490b7e16ae4cf9366cdc6dd5d52d0a
나혜승 <nahs00122@gmail.com>
2023-02-12 20:24:08 +0900

    STYLE: 상품이 없을 때 문구 추가해주기

--- frontend/src/pages/SellList.jsx
+++ frontend/src/pages/SellList.jsx
@@ -79,13 +79,13 @@ export default function SellList() {
         <div className={styles.status}>
         <ul>
           {/**filtered된 product리스트 productitem에 보여주기 */}
-          {filtered?.map((product) => (
+          {filtered ? filtered.map((product) => (
             <ProductItem
               key={product.productId}
               product={product}
               clickProduct={clickProduct}
             />
-          ))}
+          )) : <div>상품이 없어여</div>} 
         </ul>
       </div>
       <BottomNav />   
