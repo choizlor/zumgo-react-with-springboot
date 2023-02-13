c2a916cb5f5cb42d6e2f3b9795f4cc71e9569955
한선영 <gyeoul98@naver.com>
2023-02-13 11:07:59 +0900

    FIX: 알림톡 token 발급 에러 해결

--- backend/src/main/java/com/isf6/backend/service/TalkService.java
+++ backend/src/main/java/com/isf6/backend/service/TalkService.java
@@ -1,11 +1,14 @@
 package com.isf6.backend.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.isf6.backend.common.oauth.OauthToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,39 +16,49 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class TalkService {
 
     //알림톡 전송을 위한 token 발급
-    public String createToken() {
+    public String createToken() throws JsonProcessingException, ParseException {
         RestTemplate rt = new RestTemplate();
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("apikey", "ivzxf96trcesudys8du2ib7pa3kizcij");
-        params.add("userid", "gyeoul98");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params);
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("apikey", "ivzxf96trcesudys8du2ib7pa3kizcij"); //api key
+        params.add("userid", "gyeoul98"); //사이트 아이디
+        HttpEntity<MultiValueMap<String, Object>> TokenRequest = new HttpEntity<>(params);
 
         //7일 동안 사용 가능한 엑세스 토큰을 발급 받기
         ResponseEntity<String> accessTokenResponse = rt.exchange(
                 "https://kakaoapi.aligo.in/akv10/token/create/7/d/",
                 HttpMethod.POST,
-                kakaoTokenRequest,
+                TokenRequest,
                 String.class
         );
 
-        //5.
-        ObjectMapper objectMapper = new ObjectMapper();
-        OauthToken oauthToken = null;
-        try {
-            oauthToken = objectMapper.readValue(accessTokenResponse.getBody(), OauthToken.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+        String token = null;
+        log.info("access : {}", accessTokenResponse.getBody());
+        log.info("type : {}", accessTokenResponse.getBody().getClass().getName());
 
-        return oauthToken.getAccess_token();
+        Object obj = null;
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObj = new JSONObject();
+
+        obj = jsonParser.parse(accessTokenResponse.getBody());
+        jsonObj = (JSONObject) obj;
+
+        log.info("jsonObj : {}", jsonObj);
+        token = (String) jsonObj.get("urlencode");
+
+        return token;
     }
+
 
 }
