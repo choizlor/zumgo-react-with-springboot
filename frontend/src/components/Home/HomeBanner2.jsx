b012dccc903dfe2727f9bc54f0e056e379733060
yuna <yunae426@gmail.com>
2023-02-13 22:37:27 +0900

    STYLE: banner css 수정

--- frontend/src/components/Home/HomeBanner2.jsx
+++ frontend/src/components/Home/HomeBanner2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styles from "./HomeBanner.module.css";
+import styles from "./HomeBanner2.module.css";
 
 export default function HomeBanner() {
   const navigate = useNavigate();
