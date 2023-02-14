8dfea1621b4cf8b567b127162bd151adc38c2659
yuna <yunae426@gmail.com>
2023-02-14 11:56:12 +0900

    STYLE: 최근 검색어 css 수정

--- frontend/src/pages/Search/SearchItems.jsx
+++ frontend/src/pages/Search/SearchItems.jsx
@@ -1,19 +1,44 @@
 import React from "react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import styles from "./SearchItems.module.css";
 
 export default function SearchItems({ recents, searchProducts }) {
-   
-  return (
-    <div className={styles.body}>
-      <span className={styles.text}>최근 검색어</span>
-      <div className={styles.wordcontainer}>
-        {recents?.map((word, idx) => (
-          <div key={idx} className={styles.word} >
-            <span onClick={() => {searchProducts(word)}}>{word}</span>
-          </div>
-        ))}
+  if (recents.length > 10) {
+    return (
+      <div className={styles.body}>
+        <span className={styles.text}>최근 검색어</span>
+        <div className={styles.wordcontainer}>
+          {recents?.slice(0, 10).map((word, idx) => (
+            <div key={idx} className={styles.word}>
+              <span
+                onClick={() => {
+                  searchProducts(word);
+                }}
+              >
+                {word}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.body}>
+        <span className={styles.text}>최근 검색어</span>
+        <div className={styles.wordcontainer}>
+          {recents?.map((word, idx) => (
+            <div key={idx} className={styles.word}>
+              <span
+                onClick={() => {
+                  searchProducts(word);
+                }}
+              >
+                {word}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
