ae2d4b41caa8792745f05ac93d612a5a70f8ebc0
choizlor <choizlor@gmail.com>
2023-02-17 00:57:12 +0900

    FIX: timer 수정

--- frontend/src/components/Auction/Timer.jsx
+++ frontend/src/components/Auction/Timer.jsx
@@ -2,76 +2,69 @@ import React, { useEffect, useState } from "react";
 import styles from "../Auction/Timer.module.css";
 
 export default function Timer({
-  realSeconds
-  // seconds,
-  // setSeconds,
-  // currentSession,
-  // bidders,
-  // setPriceOpen,
-  // bidCount,
-  // setCelebrity,
-  // setNonCelebrity,
-  // sellerCheck,
-  // setTimerOpen,
-  // setSellerCheck,
+  seconds,
+  setSeconds,
+  currentSession,
+  bidders,
+  setPriceOpen,
+  bidCount,
+  setCelebrity,
+  setNonCelebrity,
+  sellerCheck,
+  setTimerOpen,
+  setSellerCheck,
 }) {
-  // const sendCount = () => {
-  //   currentSession
-  //     .signal({
-  //       data: seconds,
-  //       type: "timer",
-  //     })
-  //     .then(() => {
-  //       console.log("timer send")
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   if (seconds > 0) {
-  //     sendCount();
-  //   }
-  // }, [seconds])
+  const sendCount = () => {
+    currentSession
+      .signal({
+        data: seconds,
+        type: "timer",
+      })
+      .then(() => {
+        console.log("timer send")
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
                                              
-  // useEffect(() => {
-  //   if (seconds > 0) {
-  //     setTimerOpen(true);
-  //     setSellerCheck(true);
-  //     sendCount();
-  //   }
-  //   const countdown = setInterval(() => {
-  //     if (seconds > 0) {
-  //       setSeconds((prevSeconds) => {
-  //         return prevSeconds - 1;
-  //       });
-  //     }
-  //     // 0이 되면 카운트가 멈춤
-  //     if (seconds === 0) {
-  //       clearInterval(countdown);
-  //       setTimerOpen(false);
+  useEffect(() => {
+    if (seconds > 0) {
+      setTimerOpen(true);
+      setSellerCheck(true);
+      sendCount();
+    }
+    const id = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((prevSeconds) => {
+          return prevSeconds - 1;
+        });
+      }
+      // 0이 되면 카운트가 멈춤
+      if (seconds === 0) {
+        clearInterval(id);
+        setTimerOpen(false);
 
-  //       if (bidders === 0 && sellerCheck) {
-  //         setNonCelebrity(true);
-  //       }
-  //       if (bidders === 1) {
-  //         setCelebrity(true);
-  //       }
-  //       if (bidders >= 1) {
-  //         setPriceOpen(true);
-  //       }
-  //       if (bidCount > 0) {
-  //         setCelebrity(true);
-  //       }
-  //     }
-  //   }, 1000);
-  //   return () => clearInterval(countdown);
-  // }, [seconds]);
+        if (bidders === 0 && sellerCheck) {
+          setNonCelebrity(true);
+        }
+        if (bidders === 1) {
+          setCelebrity(true);
+        }
+        if (bidders >= 1) {
+          setPriceOpen(true);
+        }
+        if (bidCount > 0) {
+          setCelebrity(true);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(id);
+  }, [seconds]);
 
   return (
     <div className={styles.timer}>
-      <span className={styles.count}>{realSeconds}</span>
+      <span className={styles.count}>{seconds}</span>
     </div>
   );
 }
